df4723856231788d8fac9f39cab1da8de645a5f2
Jeff DeSouza <jeffdesouzadev@gmail.com>
2022-06-22 03:31:18 -0500

    refactor complete

--- frame-totals-improved.js
+++ frame-totals-improved.js
@@ -1,5 +1,4 @@
-const getFrameTotals = function(bowlingRolls) {
-  /*
+const getFrameTotals = function(bowlingRolls) {/*
   Assuming each element of rolls is a throw,
   irrespective of what frame the player is on.
   e.g. [4, 5, X, 3, /]
@@ -12,13 +11,10 @@ const getFrameTotals = function(bowlingRolls) {
   let getOneFrameTotal = function(rolls) {
     //returns [frameScore, rolls-with-used-indexes-snipped-off]
     //note: NOT using switch statements, since it's so easy to forget breaks
-    // let runningTotal = 0;
     if (rolls.length < 1) {
       throw new Error('getOneFrameTotal called with empty rolls array.  How??');
     } else {
-      // if (rolls[0] === '/') { //BAD INPUT
-      //   throw new RangeError("Can not start a frame with a spare.");
-      // } else {
+
       let val = rolls[0]
       let nextVal = getNextValue(rolls)
       let nextNextVal = getNextNextValue(rolls)
@@ -49,7 +45,7 @@ const getFrameTotals = function(bowlingRolls) {
       } else if (val === '/' && nextVal === '/' && nextNextVal === null ) {
           throw new RangeError("Can not start a frame with a spare.");
       } else if (!isNaN(val) && nextVal === '/' && nextNextVal === null ) {
-          const runningTotal = 10
+          const runningTotal = null
           const newRolls = rolls.slice(2, rolls.length)
           return [runningTotal, newRolls]
       } else if (val === 'X' && !isNaN(nextVal) && nextNextVal === null ) {
@@ -81,7 +77,7 @@ const getFrameTotals = function(bowlingRolls) {
       } else if (val === '/' && nextVal === '/' && nextNextVal === 'X' ) {
           throw new RangeError("Can not start a frame with a spare.");
       } else if (!isNaN(val) && nextVal === '/' && nextNextVal === 'X' ) {
-          const runningTotal = 10
+          const runningTotal = 20
           const newRolls = rolls.slice(2, rolls.length)
           return [runningTotal, newRolls]
       } else if (val === 'X' && !isNaN(nextVal) && nextNextVal === 'X' ) {
@@ -151,63 +147,6 @@ const getFrameTotals = function(bowlingRolls) {
           const newRolls = rolls.slice(2, rolls.length)
           return [runningTotal, newRolls]
       }
-
-
-      // if (val === 'X') { //STRIKE START
-      //   if (nextVal === null || nextNextVal === null) {
-      //     runningTotal = null
-      //     let newRolls = rolls.slice(1, rolls.length)
-      //     return [runningTotal, newRolls]
-      //   } else {
-      //     if (nextVal==='X') {
-      //       nextVal = 10
-      //     } else if (nextVal ==='/') {
-      //       throw new RangeError("Can not start a frame with a spare.");
-      //     }
-      //     if (nextNextVal === 'X') {
-      //       nextNextVal = 10
-      //     } else if (nextNextVal === '/') {
-      //       nextNextVal = 10-nextVal
-      //     }
-      //     runningTotal = 10 + nextVal + nextNextVal
-      //     let newRolls = rolls.slice(1, rolls.length)
-      //     return [runningTotal, newRolls]
-      //   }
-
-      // } else if (!isNaN(val)) { //NUMBER START
-      //   if (nextVal === null) {
-      //     return [null, []]
-      //   } else {
-      //     if (!isNaN(nextVal)) { //NUMBER + NUMBER
-      //       runningTotal = val + nextVal
-      //       let newRolls = rolls.slice(2, rolls.length)
-      //       return [runningTotal, newRolls]
-
-      //       //NUMBER + / + {null, NUMBER, X}
-      //     } else if (nextVal === '/' && nextNextVal === null) {
-      //       runningTotal = null
-      //       let newRolls = rolls.slice(2, rolls.length)
-      //       return [runningTotal, newRolls]
-      //     }else if (nextVal === '/' && !isNaN(nextNextVal)) {
-      //       runningTotal = 10 + nextNextVal
-      //       let newRolls = rolls.slice(2, rolls.length)
-      //       return [runningTotal, newRolls]
-      //     } else if (nextVal === '/' && nextNextVal === 'X') {
-      //       runningTotal = 10 + 10
-      //       let newRolls = rolls.slice(2, rolls.length)
-      //       return [runningTotal, newRolls]
-
-      //       //NUMBER + X
-      //     } else if (nextVal === 'X') {
-      //       throw new RangeError("Can not end a frame with a strike.");
-      //     }
-      //   }
-      // }
-
-
-
-
-      //}
     }
   }
 
